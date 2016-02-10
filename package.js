2b73d2eff6a2aeb8ab95de41845ee882bb0696b4
unknown <dnimon@mentortek.com>
2016-02-10 13:49:35 -0600

    version update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dnimon:outdated-browser',
-  version: '0.1.2',
+  version: '0.1.3',
   summary: 'Detects outdated browsers and advises users to upgrade to a new version.',
   git: 'https://github.com/dnimon/Meteor-outdated-browser.git',
   documentation: 'README.md'
